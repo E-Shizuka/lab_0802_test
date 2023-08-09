9a00ebff0f47702034307aa5f31190cfdab3d79a
E-Shizuka <snowman.r@gmail.com>
2023-08-09 11:14:03 +0900

    4th commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,8 +11,8 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div>
-      <h1 className={style.top}>INTERVIEW</h1>
-      <p className={style.news_sub}>起業家のインタビュー</p>
+      <h1 className={style.top}>つれづれ日記</h1>
+      <p className={style.news_sub}>コーギーとのほのぼの過ごす日々</p>
       <SimpleSlider /> {/* SimpleSlider コンポーネントの追加 */}
     </div>
   );
@@ -31,57 +31,91 @@ export function SimpleSlider() {
     <div className={style.toko_area}>
       <Slider {...settings}>
         <div className={style.toko_box}>
-          <div className={style.picture_box}></div>
+          <div className={style.picture_box}>
+            <Image
+              src="/corgi_1.png"
+              alt="Corgi Image"
+              width={240}
+              height={160}
+            />
+          </div>
           <div className={style.text_title}>
-            <p className={style.p_a}>INTERVIEW</p>
+            <p className={style.p_a}>今日のできごと</p>
             <p className={style.p_a}>日付</p>
           </div>
           <div className={style.text_box}></div>
         </div>
         <div className={style.toko_box}>
-          <div className={style.picture_box}></div>
+          <div className={style.picture_box}>
+            <Image
+              src="/corgi_2.png"
+              alt="Corgi Image"
+              width={240}
+              height={160}
+            />
+          </div>
           <div className={style.text_title}>
-            <p className={style.p_a}>INTERVIEW</p>
+            <p className={style.p_a}>今日のできごと</p>
             <p className={style.p_a}>日付</p>
           </div>
           <div className={style.text_box}></div>
         </div>
         <div className={style.toko_box}>
-          <div className={style.picture_box}></div>
+          <div className={style.picture_box}>
+            <Image
+              src="/corgi_4.png"
+              alt="Corgi Image"
+              width={240}
+              height={160}
+            />
+          </div>
           <div className={style.text_title}>
-            <p className={style.p_a}>INTERVIEW</p>
+            <p className={style.p_a}>今日のできごと</p>
             <p className={style.p_a}>日付</p>
           </div>
           <div className={style.text_box}></div>
         </div>
         <div className={style.toko_box}>
-          <div className={style.picture_box}></div>
+          <div className={style.picture_box}>
+            <Image
+              src="/corgi_5.jpeg"
+              alt="Corgi Image"
+              width={240}
+              height={160}
+            />
+          </div>
           <div className={style.text_title}>
-            <p className={style.p_a}>INTERVIEW</p>
+            <p className={style.p_a}>今日のできごと</p>
             <p className={style.p_a}>日付</p>
           </div>
           <div className={style.text_box}></div>
         </div>
         <div className={style.toko_box}>
-          <div className={style.picture_box}></div>
+          <div className={style.picture_box}>
+            <Image
+              src="/corgi_6.png"
+              alt="Corgi Image"
+              width={240}
+              height={160}
+            />
+          </div>
           <div className={style.text_title}>
-            <p className={style.p_a}>INTERVIEW</p>
+            <p className={style.p_a}>今日のできごと</p>
             <p className={style.p_a}>日付</p>
           </div>
           <div className={style.text_box}></div>
         </div>
         <div className={style.toko_box}>
-          <div className={style.picture_box}></div>
-          <div className={style.text_title}>
-            <p className={style.p_a}>INTERVIEW</p>
-            <p className={style.p_a}>日付</p>
+          <div className={style.picture_box}>
+            <Image
+              src="/corgi_8.jpg"
+              alt="Corgi Image"
+              width={240}
+              height={160}
+            />
           </div>
-          <div className={style.text_box}></div>
-        </div>
-        <div className={style.toko_box}>
-          <div className={style.picture_box}></div>
           <div className={style.text_title}>
-            <p className={style.p_a}>INTERVIEW</p>
+            <p className={style.p_a}>今日のできごと</p>
             <p className={style.p_a}>日付</p>
           </div>
           <div className={style.text_box}></div>
